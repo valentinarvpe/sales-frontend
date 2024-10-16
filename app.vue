fedf7f9c4cc8d8c8499a6710116dc66ae176ff2e
valentinarvpe <valentinarvpe@gmail.com>
2024-10-16 14:58:40 -0500

    Creación del proyecto

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  // No necesitas importar Vuetify directamente aquí.
+</script>
+
+<template>
+  <v-app>
+    <!-- Contenido de tu aplicación -->
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>Bienvenido a tu aplicación Nuxt con Vuetify</h1>
+            <toolbar></toolbar>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>Bienvenido a la Página Principal</h2>
+      <p>Este es el contenido de la página principal. </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  methods : {
+
+    enviarCOrreo() {
+      console.log('enviar correo');
+      
+    }
+  }
+  }
+  </script>
+  
