2e98df6b3fe3cc14bb17c6094b9061e9527ae796
valentinarvpe <valentinarvpe@gmail.com>
2024-10-16 15:27:54 -0500

    Ajuste app.vue

--- app.vue
+++ app.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-  // No necesitas importar Vuetify directamente aquí.
+// No necesitas importar Vuetify directamente aquí.
 </script>
 
 <template>
   <v-app>
     <!-- Contenido de tu aplicación -->
     <v-main>
+      <toolbar></toolbar>
       <v-container>
-        <v-row>
-          <v-col>
-            <h1>Bienvenido a tu aplicación Nuxt con Vuetify</h1>
-            <toolbar></toolbar>
-          </v-col>
-        </v-row>
+        <NuxtPage />
       </v-container>
     </v-main>
   </v-app>
